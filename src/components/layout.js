2b0a745f8b198d94780f723bdb6d938a5167a39d
Kostas Dimakis <6181485+KostasDimakis@users.noreply.github.com>
2022-09-30 16:30:44 +0300

    get rid of built with gatsby footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,27 +4,24 @@ import { Link } from "gatsby"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
 
   if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
+    return (
+      <div className="global-wrapper">
+        <main>{children}</main>
+      </div>
     )
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-    </div>
+      <div className="global-wrapper">
+        <header className="global-header">
+          <Link className="header-link-home" to="/">
+            {title}
+          </Link>
+        </header>
+        <main>{children}</main>
+      </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -12,17 +11,13 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <Bio />
-        <p>
-          No blog posts yet! Check back later.
-        </p>
+        <p>No blog posts yet! Check back later.</p>
       </Layout>
     )
   }
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
